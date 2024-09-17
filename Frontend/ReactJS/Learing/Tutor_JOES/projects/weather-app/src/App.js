19945a24dffd3c2e6f92343f1daaff99919c896c
harish_selva <harishselvaraj27@gmail.com>
2024-09-17 22:54:01 +0530

    made some initial setup

--- Frontend/ReactJS/Learing/Tutor_JOES/projects/weather-app/src/App.js
+++ Frontend/ReactJS/Learing/Tutor_JOES/projects/weather-app/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import clear from './assets/clear.png'
+import cloud from './assets/cloud.png'
+import drizzle from './assets/drizzle.png'
+import humidity from './assets/humidity.png'
+import rain from './assets/rain.png'
+import search from './assets/search.png'
+import snow from './assets/snow.png'
+import wind from './assets/wind.png'
 
 
 
 const App = () => {
   return (
     <>
-      
+      <div className='container'>
+        <div className='input-container'>
+          <input type="text" placeholder='Enter City Name' className='cityInput'/>
+          <div className='search-icon'><img src={search} alt='search'/></div>
+        </div>
+      </div>
     </>
   )
 }
